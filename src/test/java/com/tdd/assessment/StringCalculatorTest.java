98ed8922925f04111c72ee6224ccb8107832ad52
chirag.gajera <chirag4111995gajera@gmail.com>
2024-03-23 16:53:02 +0530

    1. Calling add with a negative number will throw an exception "negative numbers not allowed <negative_number>". 2.Numbers bigger than 1000 should be ignored
    Line Code-Coverage 100%

--- src/test/java/com/tdd/assessment/StringCalculatorTest.java
+++ src/test/java/com/tdd/assessment/StringCalculatorTest.java
@@ -5,17 +5,27 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class StringCalculatorTest {
-    StringCalculator stringCalculator;
+
+    private StringCalculator stringCalculator;
+
+    @Mock
+    private StringCalculator stringCalculatorMock;
 
     @BeforeEach
     public void init() {
         stringCalculator = new StringCalculator();
+        MockitoAnnotations.openMocks(this);
     }
 
     @AfterEach
@@ -45,8 +55,8 @@ public class StringCalculatorTest {
     }
 
     @Test
-    @DisplayName("Test New Line")
-    public void testNewLine() {
+    @DisplayName("Test New Line Delimiter")
+    public void testNewLineDelimiter() {
         assertEquals(10, stringCalculator.add("1\n2,3\n4"));
     }
 
@@ -56,4 +66,26 @@ public class StringCalculatorTest {
         assertEquals(15, stringCalculator.add("//;\n1;2;3;4;5"));
     }
 
+    @Test
+    @DisplayName("Test Negative Number")
+    public void testNegativeNumber() {
+        try {
+            stringCalculator.add("10,-20,30,-10");
+        } catch (IllegalArgumentException e) {
+            assertEquals(e.getMessage(), "Negative numbers not allowed: -20,-10");
+        }
+    }
+
+    @Test
+    @DisplayName("Test Over Thousand")
+    public void testOverThousand() {
+        assertEquals(22, stringCalculator.add("1000,10,12,1001"));
+    }
+
+    @Test
+    void testAdd_ConvertToIntThrowsNumberFormatException_ReturnsZero() {
+        when(stringCalculatorMock.add(anyString())).thenCallRealMethod();
+        when(stringCalculatorMock.convertToInt(anyString())).thenThrow(NumberFormatException.class);
+        assertThrows(NumberFormatException.class, () -> stringCalculatorMock.add("1,3a"));
+    }
 }
