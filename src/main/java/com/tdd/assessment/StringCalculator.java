3a34ac782f7f8d41cfd7860e6e2a420b0cba1c49
chirag.gajera <chirag4111995gajera@gmail.com>
2024-09-17 20:58:09 +0530

    1. Calling add with a negative number will throw an exception "negati… …ve numbers not allowed <negative_number>".
    2.Numbers bigger than 1000 should be ignored
    
    Line Code-Coverage 100%
    
    Code also Update using Java 8

--- src/main/java/com/tdd/assessment/StringCalculator.java
+++ src/main/java/com/tdd/assessment/StringCalculator.java
@@ -2,36 +2,52 @@ package com.tdd.assessment;
 
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.Arrays;
+
 public class StringCalculator {
 
     public int add(String numbers) {
-        if (!StringUtils.isBlank(numbers)) {
-            String delimiter = ",";
-            if (numbers.matches("//(.*)\n(.*)")) {
-                delimiter = Character.toString(numbers.charAt(2));
-                numbers = numbers.substring(4);
-            }
-
-            String[] numList = splitNumbers(numbers, delimiter + "|\n");
-            return sum(numList);
+        if (StringUtils.isBlank(numbers)) {
+            return 0;
         }
-        return 0;
+
+        String delimiter = ",";
+        if (numbers.matches("//(.*)\n(.*)")) {
+            delimiter = Character.toString(numbers.charAt(2));
+            numbers = numbers.substring(4);
+        }
+
+        String[] numberArray = splitNumbers(numbers, delimiter + "|\n");
+        return sum(numberArray);
     }
 
     private String[] splitNumbers(String numbers, String delimiter) {
         return numbers.split(delimiter);
     }
 
-
     private int sum(String[] numbers) {
-        int total = 0;
-        for (String number : numbers) {
-            total += convertToInt(number);
+        StringBuilder negativeNumbers = new StringBuilder();
+        int total = Arrays.stream(numbers)
+                .mapToInt(this::convertToInt)
+                .peek(num -> {
+                    if (num < 0) {
+                        if (negativeNumbers.length() == 0) {
+                            negativeNumbers.append(num);
+                        } else {
+                            negativeNumbers.append(",").append(num);
+                        }
+                    }
+                })
+                .filter(num -> num < 1000)
+                .sum();
+
+        if (negativeNumbers.length() > 0) {
+            throw new IllegalArgumentException("Negative numbers not allowed: " + negativeNumbers);
         }
         return total;
     }
 
-    private int convertToInt(String num) {
+    int convertToInt(String num) {
         return Integer.parseInt(num);
     }
 }
